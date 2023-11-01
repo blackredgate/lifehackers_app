0f07f7e38389b3aa2142baa2a40b223bf52c2e84
blackredgate <138416861+blackredgate@users.noreply.github.com>
2023-11-01 13:08:11 +0000

    [Add]管理者コントローラーアクション記述

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,28 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.page(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = user.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "会員の更新に成功しました"
+      redirect_to admin_user_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:customer).permit(:user_name, :email, :is_deleted)
+  end
 end
